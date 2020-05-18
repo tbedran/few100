40d03fdb2d1a3e333fecfc245591f8c88f4f335e
Thomas Bedran <tbedran@gmail.com>
2020-05-18 15:49:26 -0400

    done with day 1

---
+++ spec/00_types.spec.ts
@@ -0,0 +1,77 @@
+describe('types in typescript', () => {
+    describe('declaring variables and constants', () => {
+        it('implicit any', () => {
+            let x;
+            x = 'Tacos';
+            expect(typeof x).toBe('string')
+
+            x = 3.19;
+            expect(typeof x).toBe('number');
+
+            x = function () { }
+            expect(typeof x).toBe('function');
+
+            x = ['dog', 'cat', 'mouse', 99, 'tacos'];
+            expect(typeof x).toBe('object');
+        });
+        it('implicit typing', () => {
+            let name = 'tom';
+            let age = 35;
+
+            age = 50;
+            name = '35';
+
+            expect(typeof age).toBe('number');
+        });
+        it('implicit union', () => {
+            const name = 'tom';
+            let x: string | number = 40;
+            let b = [];
+            const g = '12';
+            x = 'help';
+            b = [g];
+        });
+        it('has const', () => {
+            const x = 3.14;
+            x = 2;
+
+            const numbers = [1, 9, 100];
+            numbers = [1, 2, 3]
+
+            numbers[0] = 9;
+
+            const movie = { title: 'jaws', year: 1978 };
+            movie = { title: 'jaws', year: 1983 };
+            movie.year = 1983;
+
+        });
+        it('has var', () => {
+            const age = 22;
+            if (age > 21) {
+
+                // tslint:disable-next-line: no-var-keyword
+                // tslint:disable-next-line: prefer-const
+                const message = 'Old enough';
+            } else {
+                // tslint:disable-next-line: no-var-keyword
+                const message = 'Too Young';
+            }
+            expect(message).toBe('Old enough');
+        });
+
+        it('has interfaces', () => {
+
+        });
+
+
+        it('logging', () => {
+
+            const story = `I was born
+                then I died
+                the end`;
+
+            console.log(story);
+
+        });
+    });
+});
--- spec/demo_with_typescript.spec.ts
+++ spec/demo_with_typescript.spec.ts
@@ -2,6 +2,10 @@
 
 describe('writing specs in typescript', () => {
     it('is easy', () => {
-        expect(true).toBe(false);
+        expect(true).toBe(true);
+    });
+    it('i can do things', () => {
+        const a = 2, b = 3;
+        expect(a + b).toBe(5);
     });
 });
